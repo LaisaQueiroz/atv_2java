412baca9fa6dd9fa4a0cd9d09dca256e947f0277
LaisaQueiroz <queirozlaisa11@gmail.com>
2025-10-24 21:50:51 -0300

    Frase.java

--- Frase.java
+++ Frase.java
@@ -1,8 +1,9 @@
 public class Frase {
     public static void main(String[] args) {
-        String nome = "Geovanna";
+        String nome = "Laisa";
         int idade = 16;
 
         System.out.println("Olá! Meu nome é " + nome + " e eu tenho " + idade + " anos.");
     }
+
 }
